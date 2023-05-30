266fc3b54ca62866ce5acaa77076144f17069f0a
hello_jiwei <3306091@qq.com>
2023-05-31 01:47:04 +0800

    first

---
+++ precipitation-common/src/main/java/com/xjw/precipitation/common/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.xjw.precipitation.common.utils;
+
+/**
+ * @author xue
+ * @date 2023/5/31 1:10
+ */
+public class StringUtils {
+
+  public static String display() {
+      return "111";
+  }
+}
